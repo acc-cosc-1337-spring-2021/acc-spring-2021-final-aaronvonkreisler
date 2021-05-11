ed7e9c61204e14abb897fc381e362ab4c35c6faa
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-05-11 09:49:20 -0500

    question 3

--- src/shooter.cpp
+++ src/shooter.cpp
@@ -1,1 +1,24 @@
-//
+#include "shooter.h"
+
+Shooter::~Shooter(){
+  for (auto roll: rolls) {
+    delete[] roll;
+  }
+}
+
+Roll* Shooter::throw_die(Die& die1, Die& die2){
+  Roll* roll = new Roll(die1, die2);
+  
+  roll->roll_die();
+  rolls.push_back(std::move(roll));
+
+  return roll;
+}
+
+std::ostream& operator<<(std::ostream& out, Shooter& shooter) {
+  for (auto roll: shooter.rolls) {
+    out<<roll->roll_value();
+  }
+
+  return out;
+}
