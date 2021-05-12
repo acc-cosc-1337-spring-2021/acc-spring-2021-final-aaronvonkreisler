a8787240c057f22d817ab34b245124fb8236e77f
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-05-12 17:45:01 -0500

    add parenthasis to question 4 for stylistic reasons

--- src/come_out_phase.cpp
+++ src/come_out_phase.cpp
@@ -3,11 +3,11 @@
 RollOutcome ComeOutPhase::get_outcome(Roll* roll) {
   int value = roll->roll_value();
 
-  if (value == 7 || value == 11) {
+  if ((value == 7 )||( value == 11)) {
     return RollOutcome::Natural;
   }
   
-  if (value == 2 || value == 3 || value == 12) {
+  if ((value == 2 )|| (value == 3) || (value == 12)) {
     return RollOutcome::Craps;
   }
 
